dc2f8783986b48efb661cb89519b363824dd48b2
su-denghuang <mason099203@gmail.com>
2025-07-02 14:10:30 +0800

    在 items-data.js 中更新 "Loath Shroud Sacrificial Garb" 的類型，並新增 "Rage Saw Anarchic Spiritblade" 物品及其詳細屬性，以擴充物品資料庫並提升資料的完整性。

--- items-data.js
+++ items-data.js
@@ -405,7 +405,7 @@ Synthesised Item
         name: "Loath Shroud Sacrificial Garb",
         mirrorFee: "60",
         category: "armor",
-        type: "",
+        type: " Explode /Increased effect of Non-Curse",
         tags: ["Crusader Item","Redeemer Item"],   
         owner: "福爾摩匹",
         contact: "福爾摩匹",
@@ -1102,6 +1102,58 @@ Split
 Crusader Item
 Redeemer Item
 
+`
+    },{
+        id: 10,
+        league: "Mercenaries",
+        name: "Rage Saw Anarchic Spiritblade",
+        mirrorFee: "60",
+        category: "weapon",
+        type: "閃現打擊 / 12L 近戰技能",
+        tags: ["new","Shaper Item","Elder Item"],   
+        owner: "Baned_BlueBin",
+        contact: "福爾摩匹",
+        imageUrl: "./image/Rage Saw Anarchic Spiritblade.png",
+        socketimageUrl: "./image/3sL.png",
+        itemData: `Item Class: One Hand Swords
+Rarity: Rare
+Rage Saw
+Anarchic Spiritblade
+--------
+One Handed Sword
+Quality: +20% (augmented)
+Physical Damage: 79-146 (augmented)
+Critical Strike Chance: 8.38% (augmented)
+Attacks per Second: 1.97 (augmented)
+Weapon Range: 1.1 metres
+--------
+Requirements:
+Level: 70
+Str: 121
+Dex: 121
+--------
+Sockets: W-W-W 
+--------
+Item Level: 86
+--------
+品質不會增加物理傷害 (enchant)
+每 8% 品質增加 1% 攻擊速度 (enchant)
+--------
+使用此武器攻擊時 100% 物理傷害轉化為隨機元素傷害 (implicit)
+--------
+插槽中寶石被等級 20 的增加暴擊率輔助
+插槽中寶石被等級 20 的殘暴輔助
+插槽中寶石被等級 20 的快速攻擊輔助
+此物品上寶石受到等級 20 的元素傷害攻擊輔助
+插槽中的技能造成 40% 更多攻擊傷害
+插槽中位移技能不消耗魔力
+增加 131% 物理傷害
+增加 21% 攻擊速度
+增加 29% 暴擊率
+攻擊技能增加 37% 元素傷害
+--------
+Shaper Item
+Elder Item
 `
     },{
         id: 11,
